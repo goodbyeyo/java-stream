146dd9edd95d5300d9586362a4a5ee7aecac0e3b
Sangwook Hwang <yoonseol1983@gmail.com>
2022-12-11 23:36:26 +0900

    Lambda Expression, Functional Interface

---
+++ src/main/java/com/study/javastream/JavaStreamApplication.java
@@ -0,0 +1,12 @@
+package com.study.javastream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class JavaStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaStreamApplication.class, args);
+    }
+
+}
