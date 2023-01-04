75f782ef1c9b652ca4ad8c2454af0c7424c22338
Sangwook Hwang <yoonseol1983@gmail.com>
2023-01-05 00:00:29 +0900

    lazy

---
+++ src/main/java/com/study/javastream/lazy_evalution_edited/LazyEvaluation.java
@@ -0,0 +1,8 @@
+package com.study.javastream.lazy_evalution_edited;
+
+/*
+Stream 은 종결처리가 이루어지기전까지 모든 계산을 미룬다
+따라서 아래의 코드를 보면 Collector 를 하기 직전에 계산이 수행되는것을 확인 할수 있다
+ */
+public class LazyEvaluation {
+}
